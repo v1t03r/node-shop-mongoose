1ed892d729c4af8325cfb5f22efbaebc8cef62f7
“Victor <victor.pacheco@gmail.com>
2021-02-08 14:23:35 -0300

    add & fetch products

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -20,14 +20,14 @@ const notFound404 = require('./routes/404');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findByPk('6019bea6ac2ecef9b18854d1')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findByPk('6019bea6ac2ecef9b18854d1')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
--- routes/admin.js
+++ routes/admin.js
@@ -6,11 +6,11 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-// router.get('/add-product', adminController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
 // router.get('/products', adminController.getProducts);
 
-// router.post('/add-product', adminController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 // router.get('/edit-product/:productId', adminController.getEditProduct);
 
