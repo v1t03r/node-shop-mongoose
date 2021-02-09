2bc260da8af4b7d382ab71915bb2c361e843f5b4
“Victor <victor.pacheco@gmail.com>
2021-02-09 09:37:47 -0300

    add login form

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 const notFound404 = require('./routes/404');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,6 +32,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 app.use(notFound404);
 
 mongoose
