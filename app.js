0245d193e9b6463d8f1b03b9e6d6ac9157d40d09
“Victor <victor.pacheco@gmail.com>
2021-02-09 14:41:42 -0300

    update signup

--- app.js
+++ app.js
@@ -57,18 +57,6 @@ app.use(notFound404);
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Victor',
-          email: 'victor.pacheco@gmail.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
     console.log('Connected successfully!');
   })
