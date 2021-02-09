4a143374e3060679742b692d71e420ac678fe14e
“Victor <victor.pacheco@gmail.com>
2021-02-09 15:11:42 -0300

    encrypting passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -32,21 +34,25 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const consfirmPassword = req.body.confirmPassword;
+  const confirmPassword = req.body.confirmPassword;
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
-        return res.redirect('signup');
+        return res.redirect('/signup');
       }
-      const user = new User ({
-        email: email,
-        password: password,
-        cart: { items: [] }
-      });
-      return user.save();
-    })
-    .then(result => {
-      res.redirect('/');
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('/login');
+        });
     })
     .catch(err => {
       console.log(err);
